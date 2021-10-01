ed57fb0e8c43f197212b01bfc7f83e0b0f8d2c37
icsalgado <icsalgado@icloud.com>
2021-10-01 09:39:41 -0300

    Segunda forma de implementar o laço for, parecida com a forma usada em C, apenas para numeros

--- teste.sh
+++ teste.sh
@@ -2,8 +2,15 @@
 #Laço FOR
 
 #Primeira forma básica do for
-
+echo Primeiro FOR
 for variavel in 1 2 3 4 5
 do
 	echo Variavel:$variavel
+done
+
+#Segunda forma básica do for parecida com C
+echo Segundo FOR
+for (( i = 1 ; i <= 5 ; i++ ))
+do
+	echo Variavel:$i
 done
