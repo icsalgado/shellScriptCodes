8f5499d853903e1e4dcab42b0211d0a9ad403d82
icsalgado <icsalgado@icloud.com>
2021-10-07 15:52:26 -0300

    Utilizando parametros de linha de comando

---
+++ parametros.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo O primeiro parametro informado foi $1
+
+echo O quadrodo de $1 é $[$1*$1]
+
+echo O segundo parametro foi $2
+
+echo O cubo de $2 é $[$2*$2*$2]
+
+echo Foram informados $# parametros para o script $0
+
+
